386f0380efa07f4513694938c98a5ddc703b9fc6
MAC <mac@tt.local>
2026-07-05 00:12:42 +0600

    production-2

--- app/components/bottom.js
+++ app/components/bottom.js
@@ -21,31 +21,31 @@ export default function BottomMenu() {
     <Link href="/Manifesto" onClick={e=>{
     
     }}>
-      <h1 className="md:text-[1.2vw] hover:line-through decoration-[#c80000] duration-1000 p-1 text-[4vw] font-bold leading-none">
+      <h1 className="md:text-[1.2vw] hover:line-through decoration-[#c80000] duration-1000 p-1 text-[4vw] leading-none">
          Manifesto <span className="text-[#c80000] md:text-[1.2vw] text-[4vw]">*</span>
       </h1>
     </Link>
 
     <Link href="/Manifesto">
-      <h1 className="md:text-[1.2vw] hover:line-through decoration-[#c80000] p-1 text-[4vw] font-bold leading-none">
+      <h1 className="md:text-[1.2vw] hover:line-through decoration-[#c80000] p-1 text-[4vw]  leading-none">
         Random <span className="text-[#c80000] md:text-[1.2vw] text-[4vw]">*</span>
       </h1>
     </Link>
 
     <Link href="/Scratchers">
-      <h1 className="md:text-[1.2vw] hover:line-through decoration-[#c80000] p-1 text-[4vw] font-bold leading-none">
+      <h1 className="md:text-[1.2vw] hover:line-through decoration-[#c80000] p-1 text-[4vw]  leading-none">
         Scratchers <span className="text-[#c80000] md:text-[1.2vw] text-[4vw]">*</span>
       </h1>
     </Link> 
     <Link href="/garden">
     <div className='flex flex-row items-center justify-center'>
       <img src='astb.svg'className='rotate-225 pr-1 w-[20px]'/>
-      <h1 className="md:text-[1.2vw] hover:line-through decoration-[#c80000] p-1 text-[4vw] font-bold leading-none">
+      <h1 className="md:text-[1.2vw] hover:line-through decoration-[#c80000] p-1 text-[4vw] leading-none">
         Garden <span className="text-[#c80000] md:text-[1.2vw] text-[4vw]">*</span>
       </h1>
     </div>
     </Link>
-    <h1 className="md:text-[1vw] border-l border-[#c80000] pl-2 mt-2 text-[2vw] font-bold leading-none">
+    <h1 className="md:text-[1vw] text-bold border-l border-[#c80000] pl-2 mt-2 text-[2vw] leading-none">
         C.2026.SHUNNO.O.OSHIM.
       </h1>
     </div>
